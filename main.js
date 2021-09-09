ad2de089d3fbc0099977b4fe662d8123f08acee0
Adegbamiye Adedayo <adegbamiyestephen2018@gmail.com>
2021-09-09 09:24:40 -0700

    make it better

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
-const fs = require('fs');
-//const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
+const client = new Client();
 
-const SESSION_FILE_PATH = './session.json';
-
-//client.on('qr', qr => {
- //   qrcode.generate(qr, {small: true});
-//});
-// Load the session data if it has been previously saved
-let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
-
-// Use the saved values
-const client = new Client({
-    session: sessionData
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
 });
 
 client.on('ready', () => {
@@ -24,14 +12,6 @@ client.on('ready', () => {
 });
 
 client.initialize();
-client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-})
 client.on('message', message => {
 	if(message.body === 'Hello') {
 		message.reply('hello lovely');
